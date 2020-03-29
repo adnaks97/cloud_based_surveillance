6bf031d356351bce762dcf819aab3c9700ba045d
Skanda Suresh <skoct13@gmail.com>
2020-03-29 15:57:15 -0700

    new motion.py code and modified init script

--- init.sh
+++ init.sh
@@ -4,6 +4,5 @@ cd /home/ubuntu/
 git clone https://github.com/adnaks97/cloud_based_surveillance.git
 cd cloud_based_surveillance
 pip3 install -r requirements.txt
-crontab -l > crontabFile
 echo "@reboot sh /home/ubuntu/cloud_based_surveillance/detect.sh" >> crontabFile
 crontab crontabFile
